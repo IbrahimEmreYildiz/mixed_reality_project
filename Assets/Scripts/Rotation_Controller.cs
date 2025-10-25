fc2ca76ba0557fe4baafc08a465ae2e64ec96399
Ibrahim Emre Yildiz <iemreis803@gmail.com>
2025-10-26 02:11:03 +0300

    unity planets animation project

---
+++ Assets/Scripts/Rotation_Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation_Controller : MonoBehaviour
+{
+    public GameObject PlanetObject;
+    public Vector3 RotationVector;
+
+
+    private void Update()
+    {
+        PlanetObject.transform.Rotate(RotationVector * Time.deltaTime);
+    }
+}
+
